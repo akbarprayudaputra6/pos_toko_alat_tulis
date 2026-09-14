503eb6a0b25f83ba31c817bb599db9529afc6639
Akbar Prayuda <akbarprayudaputra08@gmail.com>
2026-09-14 10:30:33 +0700

    feat: get information about 'me'

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,4 +13,5 @@ Route::post('/register', [AuthController::class, 'register'])->middleware('throt
 Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
 });
